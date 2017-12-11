94ec7ecc1467d01f3d304a9ad9a7a6dad2e2dfcc
Victor Jordan <victorjordan95@gmail.com>
2017-12-11 20:32:39 -0200

    first challenge

--- script.js
+++ script.js
@@ -6,16 +6,30 @@
 
 //Lecture : opreators 
 
-var birthYear = 2016 - 26;
-console.log(birthYear);
+var player1 = {
+    height : 170,
+    age : 20,
+};
 
-var ageJohn = 30;
-var ageMark = 30;
 
-ageJohn = ageMark = (3 + 5) * 4 - 6;
+var player2 = {
+    height : 177,
+    age : 18,
+};
 
-ageJohn++;
-ageMark *= 2;
+var scoreP1 = player1.height * player1.age;
+var scoreP2 = player2.height * player2.age;
 
-console.log(ageJohn);
-console.log(ageMark);
+console.log(`Player1, ${player1.age} years old and ${player1.height}`);
+console.log(`Player1, ${player2.age} years old and ${player2.height}`);
+
+if (scoreP1 > scoreP2) {
+    console.log(`Player1, win with the score ${scoreP1}`);
+
+} else if ( scoreP1 === scoreP2 ){
+    console.log ('DRAW!');
+
+} else {
+    console.log(`Player2, win with the score ${scoreP2}`);
+}
+
