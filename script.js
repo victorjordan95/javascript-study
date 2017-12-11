c262b2cbbea9eb029eda950e3b1eb36b0a4d8e16
Victor Jordan <victorjordan95@gmail.com>
2017-12-11 13:08:40 -0200

    second lecture

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-var name = 'Victor';
-console.log(name);
+// var name = 'Victor';
+// console.log(name);
 
-var lastName = 'Jordan';
-console.log(lastName);
+// var lastName = 'Jordan';
+// console.log(lastName);
+
+//Lecture : opreators 
+
+var birthYear = 2016 - 26;
+console.log(birthYear);
+
+var ageJohn = 30;
+var ageMark = 30;
+
+ageJohn = ageMark = (3 + 5) * 4 - 6;
+
+ageJohn++;
+ageMark *= 2;
+
+console.log(ageJohn);
+console.log(ageMark);
