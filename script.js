9a56ce1fa2934b15079bd3da988e0e90f539377c
Victor Jordan <victorjordan95@gmail.com>
2017-12-11 22:52:32 -0200

    first challenge and array lecture

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
-// var name = 'Victor';
-// console.log(name);
+//CHALLENGE ONE
+// var player1 = {
+//     height : 170,
+//     age : 20,
+// };
 
-// var lastName = 'Jordan';
-// console.log(lastName);
+// var player2 = {
+//     height : 177,
+//     age : 18,
+// };
 
-//Lecture : opreators 
+// var player3 = {
+//     height : 155,
+//     age : 49
+// };
 
-var player1 = {
-    height : 170,
-    age : 20,
-};
+// var scoreP1 = player1.height + 5 * player1.age;
+// var scoreP2 = player2.height + 5 * player2.age;
+// var scoreP3 = player3.height + 5 * player3.age;
 
+// console.log(`Player1, ${player1.age} years old and ${player1.height}`);
+// console.log(`Player1, ${player2.age} years old and ${player2.height}`);
 
-var player2 = {
-    height : 177,
-    age : 18,
-};
+// if (scoreP1 > scoreP2 && scoreP1 > scoreP3) {
+//     console.log(`Player1, win with the score ${scoreP1}`);
+// } else if ( scoreP1 === scoreP2 === scoreP3){
+//     console.log ('DRAW!');
+// } else if (scoreP2 > scoreP3 && scoreP2 > scoreP1){
+//     console.log(`Player2, win with the score ${scoreP2}`);
+// } else {
+//     console.log(`Player3, win with the score ${scoreP3}`);
+// }
 
-var scoreP1 = player1.height * player1.age;
-var scoreP2 = player2.height * player2.age;
+var names = ['Victor', 'John', 'Mary'];
+var years = new Array (1990, 1999, 2002);
 
-console.log(`Player1, ${player1.age} years old and ${player1.height}`);
-console.log(`Player1, ${player2.age} years old and ${player2.height}`);
+console.log(names);
 
-if (scoreP1 > scoreP2) {
-    console.log(`Player1, win with the score ${scoreP1}`);
+var john = ['John', 'Smith', 1990, 'designer', 'false']
 
-} else if ( scoreP1 === scoreP2 ){
-    console.log ('DRAW!');
+john.push('Blue');
+john.unshift('Mr');
 
-} else {
-    console.log(`Player2, win with the score ${scoreP2}`);
+console.log(john);
+
+//IndexOf returns -1 if the array not contains 'teacher', and 1 if contains
+if (john.indexOf('teacher') === -1) {
+    console.log('John is not a teacher');
 }
-
