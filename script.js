2646cb1396569ae04d78d7106cdb360533025aad
Victor Jordan <victorjordan95@gmail.com>
2017-12-21 16:27:56 -0200

    hoisting lecture

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+function calculateAge(year) {
+    console.log(2016 - year);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+calculateAge(1995);
 
 
 ///////////////////////////////////////
