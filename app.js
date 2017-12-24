192f30b92605908ece00e4dfa2d340fbee538859
Victor Jordan <victorjordan95@gmail.com>
2017-12-24 18:16:38 -0200

    inicio do jogo

--- app.js
+++ app.js
@@ -8,3 +8,50 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+var scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    var diceDom = document.querySelector('.dice');
+    diceDom.style.display = 'block';
+    diceDom.src = `dice-${dice}.png`;
+
+    if (dice !== 1) {
+        
+        roundScore += dice;
+        document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+
+    } else {
+
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+
+    }
+
+});
+
+
+// document.querySelector('#current-' + activePlayer).innerHTML = `<em> ${dice} </em>`;
+// document.querySelector('#current-' + activePlayer).textContent = dice;
