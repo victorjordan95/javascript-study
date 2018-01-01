c6cdb28852186e5409863cf3e8b26b52db258ccf
Victor Jordan <victorjordan95@gmail.com>
2018-01-01 19:04:26 -0200

    ecmascript 2016

---
+++ section-7-ES2016/script.js
@@ -0,0 +1,34 @@
+// Lecture : Template literals
+
+// let firstName = `Victor`;
+// let lastName = `Jordan`;
+// const yearOfBirth = 1995;
+
+// function calcAge(year) {
+//     return 2018 - yearOfBirth;
+// }
+
+// console.log(`${firstName} ${lastName}, ${calcAge(yearOfBirth)} years old`);
+
+// const n = `${firstName} ${lastName}`;
+
+// console.log(n.startsWith('V'));
+// console.log(n.endsWith('k'));
+
+
+// Arrow functions
+/*
+const years = [1990, 1965, 1982, 1937];
+
+var ages5 = years.map(function(el) {
+    return 2016 - el;
+});
+console.log(ages5);
+
+var ages6 = years.map(el => 2016 - el);
+console.log(ages6);
+*/
+
+//Arrow function 2
+
+  
